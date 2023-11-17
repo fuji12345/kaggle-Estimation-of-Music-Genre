faf9e89cbc0d1c24fddb7e2ca17f62b9748eae86
Ryota Fujimura <ryotaajishio0626@icloud.com>
2023-11-17 20:32:48 +0900

    add: create outfit

---
+++ experiment/experiment.py
@@ -0,0 +1,6 @@
+class Exp:
+    def __init__(self, config) -> None:
+        pass
+
+    def run(self):
+        print("run")
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import hydra
 
+import experiment
+from experiment import Exp
+
 
 @hydra.main(config_path="conf", config_name="main")
 def main(config):
-    return
+    exp: Exp = Exp(config)
+    exp.run()
+
+
+if __name__ == "__main__":
+    main()
