fcf687ce2c3234401ba364d09ba39f93bdc1277c
Ryota Fujimura <ryotaajishio0626@icloud.com>
2023-12-12 17:18:46 +0900

    change verbose config

--- model/xgboost.py
+++ model/xgboost.py
@@ -4,13 +4,17 @@ from .utils import f1_micro
 
 
 class XGBoost:
-    def __init__(self) -> None:
+    def __init__(self, early_stopping_rounds=100) -> None:
         self.model = xgb.XGBClassifier(
-            n_estimators=10000, early_stopping_rounds=100, eval_metric=f1_micro, objective="multi:softmax", num_class=8
+            n_estimators=10000,
+            early_stopping_rounds=early_stopping_rounds,
+            eval_metric=f1_micro,
+            objective="multi:softmax",
+            num_class=8,
         )
 
-    def fit(self, X, y, eval_set):
-        self.model.fit(X, y, eval_set=eval_set)
+    def fit(self, X, y, eval_set, verbose=False):
+        self.model.fit(X, y, eval_set=eval_set, verbose=verbose)
 
     def predict(self, X):
         predict = self.model.predict(X)
