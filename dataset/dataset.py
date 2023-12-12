ea4b7b0cc1447133070747bc6219b295a00ed1e6
kusukawa-b3 <root@DESKTOP-Q3AO3JU>
2023-12-12 13:49:32 +0900

    update:

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,37 +1,53 @@
-import ssl
+# import ssl
 
 import pandas as pd
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text
+
+# import tensorflow as tf
+# import tensorflow_hub as hub
+# import tensorflow_text
 from hydra.utils import to_absolute_path
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, PolynomialFeatures, StandardScaler
 from sklearn.utils.class_weight import compute_class_weight
 
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 
 class MusicGenre:
     def __init__(self, config) -> None:
         self.config = config
 
-        self.train = pd.read_csv(to_absolute_path("datasets/train.csv"))
-        self.test = pd.read_csv(to_absolute_path("datasets/test.csv"))
+        self.train = pd.read_csv(to_absolute_path("datasets/train_new_embed.csv"))
+        self.test = pd.read_csv(to_absolute_path("datasets/test_new_embed.csv"))
 
         self.target_column = "genre"
 
-        self.columns = [x for x in self.train.columns.tolist() if x != self.target_column]
+        self.columns = [
+            x for x in self.train.columns.tolist() if x != self.target_column
+        ]
         self.use_columns = [x for x in self.columns if x not in ["ID", "type"]]
-        self.categorical_columns = ["mode", "song_name"]
-        self.contenious_columns = [x for x in self.use_columns if x not in self.categorical_columns]
+        self.categorical_columns = ["mode"]
+        self.contenious_columns = [
+            x for x in self.use_columns if x not in self.categorical_columns
+        ]
 
         self.test_id_column = self.test["ID"]
         self.drop_id_and_type()
 
-        classes = ["Dark Trap", "Emo", "Hiphop", "Pop", "Rap", "RnB", "Trap Metal", "Underground Rap"]
-        class_weights = compute_class_weight("balanced", classes=classes, y=self.train[self.target_column])
+        classes = [
+            "Dark Trap",
+            "Emo",
+            "Hiphop",
+            "Pop",
+            "Rap",
+            "RnB",
+            "Trap Metal",
+            "Underground Rap",
+        ]
+        class_weights = compute_class_weight(
+            "balanced", classes=classes, y=self.train[self.target_column]
+        )
         inverse_class_weights = 1.0 / class_weights
         self.scale_pos_weight = sum(inverse_class_weights) / len(inverse_class_weights)
 
@@ -48,7 +64,9 @@ class MusicGenre:
         return tf.reshape(summed_vector, shape=(-1,))
 
     def song_vector(self):
-        embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual/3")
+        embed = hub.load(
+            "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3"
+        )
         vectors = embed(self.train["song_name"])
         test_vectors = embed(self.test["song_name"])
         flattened_vectors = self.flatten_vector(vectors)
@@ -59,14 +77,22 @@ class MusicGenre:
         self.test.drop(["song_name"], axis=1, inplace=True)
 
     def create_polynomial_features(self):
-        features = ["instrumentalness", "danceability", "speechiness", "duration_ms", "valence"]
+        features = [
+            "instrumentalness",
+            "danceability",
+            "speechiness",
+            "duration_ms",
+            "valence",
+        ]
         new_columns_name = [f"A{x}" for x in range(15)]
         pf = PolynomialFeatures(include_bias=False).fit(self.train[features])
         train_created_features = pd.DataFrame(
-            pf.transform(self.train[features])[:, len(features) :], columns=new_columns_name
+            pf.transform(self.train[features])[:, len(features) :],
+            columns=new_columns_name,
         )
         test_created_features = pd.DataFrame(
-            pf.transform(self.test[features])[:, len(features) :], columns=new_columns_name
+            pf.transform(self.test[features])[:, len(features) :],
+            columns=new_columns_name,
         )
 
         drop_columns = ["A0", "A5", "A9", "A12", "A14"]  # 結果を確認し、貢献できていないカラムを落とす
@@ -88,15 +114,21 @@ class MusicGenre:
         pca = pca.fit(pca_train_data_standardized)
         train_principal_components = pca.transform(pca_train_data_standardized)
         test_principal_components = pca.transform(pca_test_data_standardized)
-        train_new_features = pd.DataFrame(data=train_principal_components, columns=["PC1", "PC2"])
-        test_new_features = pd.DataFrame(data=test_principal_components, columns=["PC1", "PC2"])
+        train_new_features = pd.DataFrame(
+            data=train_principal_components, columns=["PC1", "PC2"]
+        )
+        test_new_features = pd.DataFrame(
+            data=test_principal_components, columns=["PC1", "PC2"]
+        )
 
         self.train = pd.concat([self.train, train_new_features], axis=1)
         self.test = pd.concat([self.test, test_new_features], axis=1)
 
     def label_encoding(self):
         self.le = LabelEncoder()
-        self.train[self.target_column] = self.le.fit_transform(self.train[self.target_column])
+        self.train[self.target_column] = self.le.fit_transform(
+            self.train[self.target_column]
+        )
 
         """
         encoding_mapping = dict(zip(self.le.classes_, self.le.transform(self.le.classes_)))
@@ -119,8 +151,8 @@ class MusicGenre:
         if self.config.preprocessing.new_col:
             self.new_columns()
 
-        if self.config.preprocessing.song_vector:
-            self.song_vector()
-        else:
-            self.train.drop(["song_name"], axis=1, inplace=True)
-            self.test.drop(["song_name"], axis=1, inplace=True)
+        # if self.config.preprocessing.song_vector:
+        #     self.song_vector()
+        # else:
+        #     self.train.drop(["song_name"], axis=1, inplace=True)
+        #     self.test.drop(["song_name"], axis=1, inplace=True)
